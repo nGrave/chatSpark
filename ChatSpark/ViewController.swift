ad3aa65c479f4c29035759e8ab4e1fe1d26c59d6
Nathan Graves <ngraves@live.com.au>
2017-10-12 09:55:56 +0800

    Initial Commit

---
+++ ChatSpark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatSpark
+//
+//  Created by Nathan on 12/10/17.
+//  Copyright © 2017 cits3200J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
